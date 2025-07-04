cb65fcfb1dad322ce57e477133711fd28137fe79
MostafaAymanMostafaEzzat <teamt3201@gmail.com>
2025-07-04 21:07:32 +0300

    ggg

--- src/app/configure/design/design.tsx
+++ src/app/configure/design/design.tsx
@@ -172,7 +172,7 @@ export default function DesignOflient({
           />
         </div>
         <Rnd
-          className="  relative z-[2] max-md:max-w-24 text-center "
+          className="  relative z-[2] max-md:max-w-8 text-center "
           default={{ ...position, ...Dimention }}
           onDragStop={(_, { x, y }) => {
             setPosition({ x, y });
